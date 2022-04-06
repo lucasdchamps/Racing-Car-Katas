dcb7b63d4da48fb211dc05ffc5d4127e0e1f5f6b
Sébastien Canonica <sebastien.canonica@360learning.com>
2022-04-06 09:18:29 +0200

    3rd method

--- TypeScript/tire-pressure-monitoring-system-tests/alarm-test.ts
+++ TypeScript/tire-pressure-monitoring-system-tests/alarm-test.ts
@@ -1,36 +1,33 @@
 import { expect } from 'chai';
 import 'mocha';
+import * as sinon from "sinon";
 import Alarm from '../tire-pressure-monitoring-system/alarm';
 import {ISensor} from "../tire-pressure-monitoring-system/sensor";
+import {stubInterface, stubObject} from "ts-sinon";
 
 describe('Tyre Pressure Monitoring System', () => {
-
 	describe('Alarm', () => {
 		it('foo', () => {
-			const alarm = new Alarm(new FakeSensor(18));
+			const fakeSensor = stubInterface<ISensor>();
+			fakeSensor.popNextPressurePsiValue.returns(18);
+			const alarm = new Alarm(fakeSensor);
 
 			alarm.check();
 
 			expect(alarm.isAlarmOn()).eql(false);
+			expect(fakeSensor.popNextPressurePsiValue).calledOnce;
 		});
 
 		it('foo', () => {
-			const alarm = new Alarm(new FakeSensor(22));
+			const fakeSensor = stubInterface<ISensor>({
+				popNextPressurePsiValue: 22
+			});
+			const alarm = new Alarm(fakeSensor);
 
 			alarm.check();
 
 			expect(alarm.isAlarmOn()).eql(true);
 		});
-
-		class FakeSensor implements ISensor {
-			tirePressure: number;
-			constructor(tirePressure: number) {
-				this.tirePressure = tirePressure;
-			}
-			popNextPressurePsiValue(): number {
-				return this.tirePressure;
-			}
-		}
 	});
 
 });
