f463d82b75495fce8788767c3cd501b56ad1bf22
Alexis Lozano <alexis.lozano@360learning.com>
2022-04-06 09:05:57 +0200

    ticket dispenser

--- TypeScript/turn-ticket-dispenser-tests/ticket-dispenser-test.ts
+++ TypeScript/turn-ticket-dispenser-tests/ticket-dispenser-test.ts
@@ -1,17 +1,27 @@
 import { expect } from 'chai';
 import 'mocha';
-import TicketDispenser from '../turn-ticket-dispenser/ticket-dispenser';
+import TicketDispenser, {INumberGenerator} from '../turn-ticket-dispenser/ticket-dispenser';
 
 describe('Turn Ticket Dispenser', () => {
 
 	describe('TurnTicketDispenser', () => {
 
-		it.skip('foo', () => {
+		it('foo', () => {
+			const n = 18;
 			const dispenser = new TicketDispenser();
-			const ticket = dispenser.getTurnTicket();
-			expect(ticket.getTurnNumber()).to.eql(-1);
+			const ticket = dispenser.getTurnTicket(new TestableNumberGenerator(n));
+			expect(ticket.getTurnNumber()).to.eql(n);
 		});
 
+		class TestableNumberGenerator implements INumberGenerator {
+			private readonly n: number;
+			constructor(n: number) {
+				this.n = n;
+			}
+			public getNextNumber(): number {
+				return this.n;
+			}
+		}
 	});
 
 });
--- TypeScript/turn-ticket-dispenser/ticket-dispenser.ts
+++ TypeScript/turn-ticket-dispenser/ticket-dispenser.ts
@@ -3,11 +3,21 @@ import TurnTicket from './turn-ticket';
 
 export default class TicketDispenser {
 
-	public getTurnTicket() {
-		const newTurnNumber = TurnNumberSequence.getNextTurnNumber();
+	public getTurnTicket(generator: INumberGenerator = new NumberGenerator()) {
+		const newTurnNumber = generator.getNextNumber();
 		const newTurnTicket = new TurnTicket(newTurnNumber);
 
 		return newTurnTicket;
 	}
 
+}
+
+export interface INumberGenerator {
+	getNextNumber(): number;
+}
+
+class NumberGenerator implements INumberGenerator {
+	public getNextNumber() {
+		return TurnNumberSequence.getNextTurnNumber();
+	}
 }
